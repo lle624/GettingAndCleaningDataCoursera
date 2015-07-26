ecd8b7a3d0539f009079ef730a039b14419b3ecb
lle624 <lle624@users.noreply.github.com>
2015-07-26 17:13:18 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,9 @@
+# If the package "plyr" does not already exist, then install.packages("plyr")
+# Load the necessary libraries
+library(plyr)
+
 # Obtain the data and unzip the data
+filename <- "getdata_dataset.zip"
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
   download.file(fileURL, filename, method="curl")
@@ -7,6 +12,37 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(filename) 
 }
 
-# Load the necessary libraries
-library(plyr)
+# 1. Merge the training and test sets to create one data set
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+subject_data <- rbind(subject_train, subject_test)
+
+# 2. Extract only the measurements on the mean and standard deviation for each measurement
+features <- read.table("features.txt")
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+x_data <- x_data[, mean_and_std_features]
+names(x_data) <- features[mean_and_std_features, 2]
+
+# 3. Use descriptive activity names to name the activities in the data set
+activities <- read.table("activity_labels.txt")
+y_data[, 1] <- activities[y_data[, 1], 2]
+names(y_data) <- "activity"
+
+# 4. Appropriately label the data set with descriptive variable names
+names(subject_data) <- "subject"
+
+all_data <- cbind(x_data, y_data, subject_data)
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+write.table(averages_data, "averages_data.txt", row.name=FALSE)
 
