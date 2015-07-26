67d57aa74233c61e9f724f2be1edb1a3e30b6ca7
lle624 <lle624@users.noreply.github.com>
2015-07-26 16:56:01 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,12 @@
+# Obtain the data and unzip the data
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+# Load the necessary libraries
+library(plyr)
 
